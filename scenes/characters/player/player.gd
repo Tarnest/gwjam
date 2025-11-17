2a6d7480902cb5b08aec16d71c757997b7ca7b05
Tarnest <tristanconnolly1@gmail.com>
2025-11-16 22:26:57 -0500

    Added Animations

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -2,8 +2,9 @@ class_name Player
 extends CharacterBody2D
 
 @onready var hit_area: Area2D = $HitArea
+@onready var sprite: Sprite2D = $Sprite2D
 
-var speed: int = 200
+var speed: int = 150
 var last_direction: Vector2 = Vector2.ZERO
 var direction: Vector2:
 	set(value):
--- scenes/characters/player/states/idle.gd
+++ scenes/characters/player/states/idle.gd
@@ -1,10 +1,45 @@
 extends State
 
 @export var player: Player
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+var up_left: Vector2 = Vector2.UP + Vector2.LEFT
+var up_right: Vector2 = Vector2.UP + Vector2.RIGHT
+var down_left: Vector2 = Vector2.DOWN + Vector2.LEFT
+var down_right: Vector2 = Vector2.DOWN + Vector2.RIGHT
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("move") || Input.is_action_just_pressed("swing"):
 		state_finished.emit()
-
+	
+	animate()
+	
 func animate():
-	pass
+	match player.last_direction.round():
+		Vector2.UP:
+			player.sprite.flip_h = false
+			animation_player.play("idle_up")
+		up_right:
+			player.sprite.flip_h = false
+			animation_player.play("idle_up")
+		up_left:
+			player.sprite.flip_h = false
+			animation_player.play("idle_up")
+		Vector2.DOWN:
+			player.sprite.flip_h = false
+			animation_player.play("idle_down")
+		down_left:
+			player.sprite.flip_h = false
+			animation_player.play("idle_down")
+		down_right:
+			player.sprite.flip_h = false
+			animation_player.play("idle_down")
+		Vector2.LEFT:
+			player.sprite.flip_h = false
+			animation_player.play("idle_side")
+		Vector2.RIGHT:
+			player.sprite.flip_h = true
+			animation_player.play("idle_side")
+		Vector2.ZERO:
+			player.sprite.flip_h = false
+			animation_player.play("idle_down")
